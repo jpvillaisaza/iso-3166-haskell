06e18485b66d638ac2b5df51cba04303ff34a67e
J. P. Villa Isaza <584947+jpvillaisaza@users.noreply.github.com>
2020-12-17 15:09:08 -0500

    Add CountryCode2

--- src/Data/Country/Code.hs
+++ src/Data/Country/Code.hs
@@ -1,47 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Data.Country.Code
+  ( CountryCode
+  )
   where
 
--- text
-import Data.Text (Text)
+-- iso3166
+import Data.Country.Code2 (CountryCode2)
 
 
-data CountryCode
-  = CA -- CAN 124
-  | CO -- COL 170
-  | ES -- ESP 724
-  | US -- USA 840
-  deriving (Bounded, Enum, Eq, Show)
-
-toName :: CountryCode -> Text
-toName countryCode =
-  case countryCode of
-    CA -> "Canada"
-    CO -> "Colombia"
-    ES -> "Spain"
-    US -> "United States of America"
-
-toName2 :: CountryCode -> (Text, Text, Text)
-toName2 countryCode =
-  case countryCode of
-    CA ->
-      ( "CANADA"
-      , "Canada"
-      , ""
-      )
-    CO ->
-      ( "COLOMBIA"
-      , "Colombia"
-      , "the Republic of Colombia"
-      )
-    ES ->
-      ( "SPAIN"
-      , "Spain"
-      , "the Kingdom of Spain"
-      )
-    US ->
-      ( "UNITED STATES OF AMERICA"
-      , "United States of America (the)"
-      , "the United States of America"
-      )
+type CountryCode =
+  CountryCode2
